bdb14e548ca49cdd2ff964a8ee597f7930703574
Thomas S <widet77@icloud.com>
2025-06-09 06:08:33 -0400

    “more”

--- pong2.py
+++ pong2.py
@@ -2,6 +2,13 @@
 import turtle
 
 t = turtle.Turtle()
-t.forward(100)
-t.right(90)
-t.forward(100)
+#t.forward(100)
+# t.right(90)
+# t.forward(100)
+
+# Screen setup
+
+win = Turtle.screen()
+win.title("Pong")
+win.bgcolor("Black")
+win.setup(width = 800, height = 600)
