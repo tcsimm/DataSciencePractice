f4cd85de4ad33718f4ab760bc9d8f29f5e4fd368
Thomas S <widet77@icloud.com>
2025-06-10 12:28:40 -0400

    “more”

--- pong3.py
+++ pong3.py
@@ -9,3 +9,9 @@ win.tracer(0)
 
 # Left paddle
 left_pad = turtle.Turtle()
+left_pad.speed(0)
+left_pad.shape("square")
+left_pad.color("white")
+left_pad.shapesize(stretch_wid=6, stretch_len=1)
+left_pad.penup()
+left_pad.goto(-350, 0)
