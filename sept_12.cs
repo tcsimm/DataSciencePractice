ffd4adde4248c5faf1361b3d47def0331b6797fe
Thomas S <widet77@icloud.com>
2025-09-12 05:40:52 -0400

    more

--- sept_12.cs
+++ sept_12.cs
@@ -7,6 +7,8 @@ class Program
         int i = 1;
         double gpa = 3.8;
         string name = "Thomas";
+
         Console.WriteLine(i); // prints 1 in terminal
+        Console.WriteLine("Gpa is" + gpa);
     }
 }
