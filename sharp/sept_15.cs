17b02faab806abcaff0acb079f895e13c27a2b78
Thomas S <widet77@icloud.com>
2025-09-15 16:09:39 -0400

    more

--- sharp/sept_15.cs
+++ sharp/sept_15.cs
@@ -1,1 +1,17 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        //variables and types
+        int x = 42;
+        string who = "Thomas";
+        double pi = 3.1415;
+        bool ok = True;
+    }
+}
