eff0d4b9fc17b251dc17d155b672c2426498952d
Thomas S <widet77@icloud.com>
2025-09-16 11:28:27 -0400

    more

--- sharp/Sept15App/Program.cs
+++ sharp/Sept15App/Program.cs
@@ -11,10 +11,14 @@ class Thomas
         float pi = 3.1415f;
         double pie = 3.1415;
 
+        bool isStudent = true;
+
         Console.WriteLine($"Hello bob, your age is {bob}.");
         Console.WriteLine($"Hi person, your name is {jim}");
         Console.WriteLine($"Pi value is {pi}");
 
         Console.WriteLine($"Double is {pie}");
+
+        Console.WriteLine($"It is {isStudent}");
     }
 }
