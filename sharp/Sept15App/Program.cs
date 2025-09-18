8008ace23cf5091f8f3c5fb02c1f291c7b75a6f0
Thomas S <widet77@icloud.com>
2025-09-18 16:40:27 -0400

    more

--- sharp/Sept15App/Program.cs
+++ sharp/Sept15App/Program.cs
@@ -7,5 +7,13 @@ class Program
     static void Main()
     {
         int n = 7;
+        if (n > 5)
+        {
+            Console.WriteLine("n > 5");
+        }
+        else
+        {
+            Console.WriteLine("not > 5");
+        }
     }
 }
--- sharp/Sept15App/obj/Debug/net9.0/Sept15App.AssemblyInfo.cs
+++ sharp/Sept15App/obj/Debug/net9.0/Sept15App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sept15App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bed750b2adefe2a4d47aa7db739a84e10f231b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b078a4d3a60bf4f7d37ad6b740c510a0c65a10f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sept15App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sept15App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
